a174f2cb17de9e75906ffc9002198f989593644a
g33kcub <g33kcub@gmail.com>
2019-05-10 09:17:30 -0400

    Update will.rb

--- plugins/fs3skills/helpers/will.rb
+++ plugins/fs3skills/helpers/will.rb
@@ -10,9 +10,9 @@ module AresMUSH
       char.update(fs3_will: will)
     end
     def self.do_regen_will(char)
-      will = @char.will
+      will = char.will
       if @char.is_approved? && will > 0
-        FS3Skills.modify_will(@char, -1)
+        FS3Skills.modify_will(char, -1)
         client.emit_ooc t('fs3magix.regen_will')
       end
     end
