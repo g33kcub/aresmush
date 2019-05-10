c0964a66dfcacbf321e38f2e3f094fcdba8e5d63
g33kcub <g33kcub@gmail.com>
2019-05-10 10:45:59 -0400

    Update will.rb

--- plugins/fs3skills/helpers/will.rb
+++ plugins/fs3skills/helpers/will.rb
@@ -13,7 +13,7 @@ module AresMUSH
       will = char.will
       if char.is_approved? && will > 0
         FS3Skills.modify_will(char, -1)
-        char.emit_ooc t('fs3magix.regen_will')
+        Login.emit_ooc_if_logged_in(char, t('fs3magix.regen_will'))
       end
     end
   end
