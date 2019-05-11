51834614bd5637059ac64340e472f91f1cedfa63
g33kcub <g33kcub@gmail.com>
2019-05-11 07:31:03 -0400

    Update group_list_template.rb

--- plugins/demographics/templates/group_list_template.rb
+++ plugins/demographics/templates/group_list_template.rb
@@ -1,13 +1,13 @@
 module AresMUSH
   module Demographics
     class GroupListTemplate < ErbTemplateRenderer
-      
+
       attr_accessor :groups
-      
+
       def initialize(groups)
-        @groups = groups.sort
+        @groups = groups.sort_by { |a| a['name'] }
         super File.dirname(__FILE__) + "/group_list.erb"
-      end      
+      end
     end
   end
 end
