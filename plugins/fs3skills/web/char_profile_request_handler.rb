e8a216b76d6866379ad0a41cade0bce20834a8a0
g33kcub <g33kcub@gmail.com>
2019-05-11 10:59:21 -0400

    webcg

--- plugins/fs3skills/web/char_profile_request_handler.rb
+++ plugins/fs3skills/web/char_profile_request_handler.rb
@@ -4,31 +4,32 @@ module AresMUSH
       def handle(request)
         char = Character.find_one_by_name request.args[:id]
         enactor = request.enactor
-        
+
         if (!char)
           return { error: t('webportal.not_found') }
         end
 
         error = Website.check_login(request, true)
         return error if error
-        
+
         if (FS3Combat.is_enabled?)
           damage = char.damage.to_a.sort { |d| d.created_at }.map { |d| {
             date: d.ictime_str,
             description: d.description,
             severity: Website.format_markdown_for_html(FS3Combat.display_severity(d.initial_severity))
-          }          
+          }
         }
       else
         damage = nil
       end
-        
+
       show_sheet = FS3Skills.can_view_sheets?(enactor) || (enactor && enactor.id == char.id)
-        
+
       if (show_sheet)
         {
           attributes: get_ability_list(char.fs3_attributes),
           action_skills: get_ability_list(char.fs3_action_skills),
+          magix: get_ability_list(char,fs3_magix_arts),
           backgrounds: get_ability_list(char.fs3_background_skills),
           languages: get_ability_list(char.fs3_languages),
           advantages: get_ability_list(char.fs3_advantages),
@@ -43,17 +44,16 @@ module AresMUSH
         }
       end
     end
-      
-    def get_ability_list(list)        
-      list.to_a.sort_by { |a| a.name }.map { |a| 
-        { 
-          name: a.name, 
-          rating: a.rating, 
+
+    def get_ability_list(list)
+      list.to_a.sort_by { |a| a.name }.map { |a|
+        {
+          name: a.name,
+          rating: a.rating,
           rating_name: a.rating_name
           }}
         end
-            
+
       end
     end
   end
-
--- plugins/fs3skills/web/chargen_info_request_handler.rb
+++ plugins/fs3skills/web/chargen_info_request_handler.rb
@@ -2,14 +2,18 @@ module AresMUSH
   module FS3Skills
     class ChargenInfoRequestHandler
       def handle(request)
-        
+
         {
           abilities: FS3Skills::AbilitiesRequestHandler.new.handle(request),
           skill_limits: Global.read_config('fs3skills', 'max_skills_at_or_above'),
           attr_limits: Global.read_config('fs3skills', 'max_attrs_at_or_above'),
+          magix_limits: Global.read_config('fs3magix', 'max_arts_at_or_above'),
+          max_arts: Global.read_config('fs3magix','max_points_on_arts'),
           max_attrs: Global.read_config('fs3skills', 'max_points_on_attrs'),
           max_action: Global.read_config('fs3skills', 'max_points_on_action'),
           min_action_skill_rating: Global.read_config('fs3skills', 'allow_unskilled_action_skills') ? 0 : 1,
+          min_magix_arts_rating: 0,
+          max_magix_arts_rating: Global.read_config('fs3magix','max_magix_rating'),
           max_skill_rating: Global.read_config('fs3skills', 'max_skill_rating'),
           max_attr_rating: Global.read_config('fs3skills', 'max_attr_rating'),
           min_backgrounds: Global.read_config('fs3skills', 'min_backgrounds'),
@@ -17,11 +21,9 @@ module AresMUSH
           free_backgrounds:  Global.read_config('fs3skills', 'free_backgrounds'),
           advantages_cost: Global.read_config("fs3skills", "advantages_cost"),
           max_ap: Global.read_config('fs3skills', 'max_ap'),
-          
-        } 
+
+        }
       end
     end
   end
 end
-
-
