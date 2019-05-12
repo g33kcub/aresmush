6327fdad336f9686068a115951410b9ea4b16d67
g33kcub <g33kcub@gmail.com>
2019-05-12 09:51:41 -0400

    Update roster_request_handler.rb

--- plugins/idle/web/roster_request_handler.rb
+++ plugins/idle/web/roster_request_handler.rb
@@ -2,48 +2,48 @@ module AresMUSH
   module Idle
     class RosterRequestHandler
       def handle(request)
-                
+
         group = Global.read_config("website", "character_gallery_group") || "Faction"
         chars = Character.all.select { |c| c.on_roster? }.group_by { |c| c.group(group) || "" }
-        
-        fields = Global.read_config("demographics", "census_fields").select { |f| f['field'] != 'name' }
+
+        fields = Global.read_config("idle", "roster_fields").select { |f| f['field'] != 'name' }
         titles = fields.map { |f| f['title'] }
-        
+
         roster = []
-        
+
         chars.each do |group, chars|
           roster << {
             name: group,
             chars: chars.map { |c| build_profile(c, fields) }
-          } 
+          }
         end
-        
+
         {
           roster: roster,
           titles: titles
         }
       end
-      
+
       def build_profile(char, field_config)
         demographics = {}
-        Demographics.basic_demographics.sort.each { |d| 
+        Demographics.basic_demographics.sort.each { |d|
             demographics[d.downcase] = char.demographic(d)
           }
-        
+
         if (Ranks.is_enabled?)
           demographics['rank'] = char.rank
         end
-          
+
         demographics['age'] = char.age
         demographics['birthdate'] = char.demographic(:birthdate)
         demographics['actor'] = char.demographic(:actor)
-        
+
         groups = {}
-        
-        Demographics.all_groups.keys.sort.each { |g| 
-          groups[g.downcase] = char.group(g)  
+
+        Demographics.all_groups.keys.sort.each { |g|
+          groups[g.downcase] = char.group(g)
           }
-        
+
         fields = {}
         field_config.each do |config|
           field = config["field"]
@@ -52,7 +52,7 @@ module AresMUSH
 
           fields[title] = Profile.general_field(char, field, value)
         end
-          
+
           {
             name: char.name,
             fullname: char.demographic(:fullname),
@@ -65,9 +65,9 @@ module AresMUSH
             contact: char.roster_contact,
             groups: groups,
             demographics: demographics,
-        }        
+        }
       end
-      
+
     end
   end
 end
