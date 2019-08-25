84b8b097cba28d28a11765b4a444c8757954dd7e
g33kcub <g33kcub@gmail.com>
2019-08-25 17:22:13 -0400

    luckgive 1.0

---
+++ plugins/luckgive/help/en/luckgive.md
@@ -0,0 +1,18 @@
+---
+toc: Community
+summary: Giving someone luck.
+aliases:
+- give_luck
+- luck_give
+---
+#Giving luck
+Give your luck to another character. Creates a record of the transaction.
+
+Luck may be given for any reason, but once it's gone, it's gone, so give wisely!
+
+`luck/give <name>=<reason>` - Give someone one of your luck points.
+`luck/record [<name>]` - See a record of all luck given to a character.
+
+---
+#Credit
+This functionality was coded by Tat@Ares Central.
--- plugins/luckgive/luckgive.rb
+++ plugins/luckgive/luckgive.rb
@@ -1,7 +1,7 @@
 $:.unshift File.dirname(__FILE__)
 
 module AresMUSH
-     module Luckgive
+     module LuckGive
 
     def self.plugin_dir
       File.dirname(__FILE__)
@@ -12,7 +12,15 @@ module AresMUSH
     end
 
     def self.get_cmd_handler(client, cmd, enactor)
-      nil
+      case cmd.root
+      when "luck"
+        case cmd.switch
+        when "give"
+          return LuckGiveCmd
+        when "record"
+          return LuckRecordCmd
+        end
+      end
     end
 
     def self.get_event_handler(event_name)
