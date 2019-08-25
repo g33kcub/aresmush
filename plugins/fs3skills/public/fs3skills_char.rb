8f65cef170f8bb2b063b868b1a1ee0fd8a2969ea
g33kcub <g33kcub@gmail.com>
2019-08-25 12:49:25 -0400

    Update fs3skills_char.rb

--- plugins/fs3skills/public/fs3skills_char.rb
+++ plugins/fs3skills/public/fs3skills_char.rb
@@ -15,7 +15,7 @@ module AresMUSH
     before_delete :delete_abilities
 
     def delete_abilities
-      [ self.fs3_attributes, self.fs3_action_skills, self.fs3_background_skills, self.fs3_languages, self.fs3_advantages, self.fs3_sorcery].each do |list|
+      [ self.fs3_attributes, self.fs3_action_skills, self.fs3_background_skills, self.fs3_languages, self.fs3_advantages, self.fs3_sorcery ].each do |list|
         list.each do |a|
           a.delete
         end
